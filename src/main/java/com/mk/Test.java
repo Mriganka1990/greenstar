51dc44828031413fd1cfe126067aae9a864c7412
mriganka <mrigankakoleyjob@gmail.com>
2019-04-23 22:16:21 +0530

    commit code

---
+++ src/main/java/com/mk/Test.java
@@ -0,0 +1,18 @@
+package com.mk;
+
+public class Test {
+
+	public static void main(String[] args){
+		
+		System.out.println("*****************");
+		System.out.println("*****************");
+		System.out.println("*****************");
+		System.out.println("*****************");
+		System.out.println("*****************");
+		System.out.println("*****************");
+		System.out.println("*****************");
+		System.out.println("*****************");
+		
+	}
+	
+}
