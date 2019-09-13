ec1555f843fc3b9175713edbcb1e372641cf89ec
Mriganka <mrigankakoleyjob@gmail.com>
2019-09-13 23:23:07 +0530

    Revert "test file has been changed"
    This reverts commit 1ab7c4ab8d9273bcedb32bd70077c3b518200a5d.

--- src/main/java/com/mk/Test.java
+++ src/main/java/com/mk/Test.java
@@ -4,7 +4,6 @@ public class Test {
 
 	public static void main(String[] args){
 		
-		System.out.println("******* THIS IS FOR TEST *********");
 		System.out.println("*****************");
 		System.out.println("*****************");
 		System.out.println("*****************");
@@ -12,7 +11,7 @@ public class Test {
 		System.out.println("*****************");
 		System.out.println("*****************");
 		System.out.println("*****************");
-		System.out.println("Mriganka Koley");
+		System.out.println("*****************");
 		
 	}
 	
